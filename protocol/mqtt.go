7287623d00155deb93e32ddfeea666578b1a597f
Xiaoju Jiang <44432198+jiang4869@users.noreply.github.com>
2023-12-21 21:08:26 +0800

    【ADD】添加对mqtt协议的支持 (#66)
    * 添加对mqtt协议的支持
    
    * Update mqtt.go
    
    ---------
    
    Co-authored-by: Kevin Wan <wanjunfeng@gmail.com>

---
+++ protocol/mqtt.go
@@ -0,0 +1,28 @@
+package protocol
+
+import (
+	"io"
+
+	"github.com/eclipse/paho.mqtt.golang/packets"
+	"github.com/kevwan/tproxy/display"
+)
+
+type mqttInterop struct {
+}
+
+func (red *mqttInterop) Dump(r io.Reader, source string, id int, quiet bool) {
+	for {
+		readPacket, err := packets.ReadPacket(r)
+		if err != nil && err == io.EOF {
+			continue
+		}
+		if err != nil {
+			display.PrintfWithTime("[%s-%d] read packet has err: %+v, stop!!!\n", source, id, err)
+			return
+		}
+		if !quiet {
+			display.PrintfWithTime("[%s-%d] %s\n", source, id, readPacket.String())
+			continue
+		}
+	}
+}
